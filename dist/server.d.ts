57e28937359f2377b546c8371a9087d31318fc9c
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2019-11-11 22:17:57 +0900

    feat(pages): add types Pages API
    close #4

--- dist/server.d.ts
+++ dist/server.d.ts
@@ -105,7 +105,7 @@ interface Schema {
     addSchema(schema: any): void;
     schema: FactoryMethods;
 }
-interface Actions extends Schema {
+interface LoadSourceActions extends Schema {
     /**
      * Add a collection.
      * @param options - GraphQL schema type name.
@@ -120,6 +120,56 @@ interface Actions extends Schema {
     getCollection(typeName: string): void;
     store: Store;
 }
+interface CreatePagesActioons {
+    /**
+     * Use the `createPages` hook if you want to create pages.
+     * Pages created in this hook will be re-created and garbage collected occasionally.
+     * Use the `createManagedPages` below to have more control over when pages are updated or deleted manually.
+     * @param options
+     * @param options.path - Required.
+     * @param options.component - Required.
+     * @param options.context - Optional context for the page and `page-query`.
+     * @param options.queryVariables - Optional context only for `page-query`.
+     */
+    createPage(options: {
+        path: string;
+        component: string;
+        context: object;
+        queryVariables: object;
+    }): void;
+}
+interface createManagedPagesActioons extends CreatePagesActioons {
+    /**
+     * Removes a page created by `createPage`.
+     * @param page
+     */
+    removePage(page: any): void;
+    /**
+     * Removes a page matching the provided path.
+     * @param path
+     */
+    removePageByPath(path: any): void;
+    /**
+     * Removes all pages matching the provided component path.
+     * @param path
+     */
+    removePagesByComponent(path: any): void;
+    /**
+     * Removes all pages matching the provided query.
+     * @param query
+     */
+    findAndRemovePages(query: any): void;
+    /**
+     * Returns all pages matching the provided query.
+     * @param query
+     */
+    findPages(query: any): void;
+    /**
+     * Returns first pages matching the provided query.
+     * @param query
+     */
+    findPage(query: any): void;
+}
 interface ServerApi {
     /**
      * The Data Store API lets you insert your own data into the GraphQL data layer.
@@ -127,8 +177,31 @@ interface ServerApi {
      * Use this API if you want to build a custom data source connection or a plugin.
      * @param callback
      */
-    loadSource(callback: (actions: Actions) => void): void;
+    loadSource(callback: (actions: LoadSourceActions) => void): void;
+    /**
+     * Create a custom GraphQL schema which will be merged with the Gridsome schema.
+     * @param callback
+     */
     createSchema(callback: (args: Schema) => void): void;
+    /**
+     * Modify or remove a node before its added to the collection.
+     * @param callback
+     */
+    onCreateNode(callback: (options: any) => void): void;
+    /**
+     * Create pages programmatically from nodes or other data.
+     * The handler for this hook will be re-executed when nodes are changed in store.
+     * Pages that are not re-created will be garbage collected.
+     * @param callback
+     */
+    createPages(callback: (options: CreatePagesActioons) => void): void;
+    /**
+     * Pages created in the `createPages` hook will be re-created and garbage collected occasionally.
+     * That's why that hook is only able to create pages.
+     * You can use a `createManagedPages` hook to create, update and remove pages yourself.
+     * @param callback
+     */
+    createManagedPages(callback: (options: createManagedPagesActioons) => void): void;
 }
 export declare type Server = (api: ServerApi) => void;
 export {};
