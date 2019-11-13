4f38544c50e9b178ed6dac8e705329ac0f64a57b
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2019-11-13 20:08:55 +0900

    perf(data store): separate Datastore API

--- dist/server.d.ts
+++ dist/server.d.ts
@@ -1,45 +1,8 @@
 import { Configuration as WebpackConfiguration } from "webpack";
 import * as ChainWebpackConfiguration from "webpack-chain";
 import { Express as ExpressConfiguration } from "express-serve-static-core";
-import { Schema } from './schema';
-interface Collection {
-    /**
-     * Add node to collection.
-     * @param object - Custom fields.
-     */
-    addNode(object: {
-        [key: string]: any;
-    }): void;
-    /**
-     * Make a root field for all nodes in collection referencing to another node.
-     * @param fieldName - The field name.
-     * @param typeName - GraphQL schema type to reference.
-     */
-    addReference(fieldName: string, typeName: string): void;
-}
-interface Store {
-    /**
-     * A helper function for creating references to other nodes when the schema types are inferred.
-     * @param typeName - The node typeName to reference or the node instance.
-     * @param id - The node id to reference (or ids if multiple nodes).
-     */
-    createReference(typeName: string | object, id: string | string[]): void;
-}
-interface LoadSourceActions extends Schema {
-    /**
-     * Add a collection.
-     * @param options - GraphQL schema type name.
-     */
-    addCollection(options: {
-        typeName: string;
-    } | string): Collection;
-    /**
-     * Get a collection previously created.
-     * @param typeName - The GraphQL schema type name.
-     */
-    getCollection(typeName: string): void;
-    store: Store;
-}
+import { Schema } from "./schema";
+import { LoadSourceActions } from "./dataStore";
 interface CreatePagesActioons {
     /**
      * Use the `createPages` hook if you want to create pages.
